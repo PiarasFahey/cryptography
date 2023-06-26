54f6fb0a6e25eb8ce90837286e3efa5caebbc34c
Piaras Fahey <faheypi@tcd.ie>
2023-06-26 19:33:30 +0100

    Initial commit

--- R/columnar_transposition.R
+++ R/columnar_transposition.R
@@ -1,7 +1,12 @@
 #' Columnar Transposition Cipher
 #'
+#' @description This can be used to create (encrypt) or solve (decrypt) a columnar transposition cipher.
+#' This method is a development of the Scytale cipher that rearranges the encryption matrix used in the
+#' Scytale method by the alphabetical ordering of the encryption key.
+#'
+#'
 #' @param message A character vector
-#' @param key An [a-zA-Z] character vector used as the encryption key
+#' @param key An "a-zA-Z" character vector used as the encryption key
 #' @param encrypt (Default: `TRUE`) TRUE will encrypt the message, while FALSE will decrypt the message.
 #'
 #' @return A character vector of either plaintext that has been encrypted or ciphertext that has been decrypted
@@ -20,56 +25,62 @@ columnar_transposition <- function(message, key, encrypt=TRUE) {
     stop("key must be an [a-zA-Z] character")
   }
 
-  # calling encryption or decryption method
+  # isolating message characters and key characters/length
+  characters <- strsplit(message, "")[[1]]
+  columns <- nchar(key)
+  key.chars <- strsplit(tolower(key), "")[[1]]
+
+  # calling encryption method
   if (encrypt == TRUE) {
-    message <- columnar_transposition_encrypt(message, key)
+
+    # checking if characters fit into the encryption matrix
+    if (length(characters) %% columns != 0)
+    {
+      # adding a blank element to the characters object to ensure encryption matrix is filled correctly
+      for (i in 1:(columns-(length(characters) %% columns)))
+      {
+        characters <- append(characters, "")
+      }
+    }
+    # placing characters into encryption matrix
+    encryption.matrix <- data.frame(matrix(characters, ncol = columns, byrow = TRUE))
+
+    # ordering encryption matrix according to key characters order
+    colnames(encryption.matrix) <- key.chars
+    encryption.matrix <- as.matrix(encryption.matrix[,order(names(encryption.matrix))])
+
+    # pasting the characters from the encryption matrix into the encrypted message
+    message <- paste(encryption.matrix, collapse = "")
   }
 
+  # calling decryption method
   if (encrypt == FALSE) {
-    message <- columnar_transposition_decrypt(message, key)
+
+    # checking if characters fit into the encryption matrix
+    if (length(characters) %% columns != 0)
+    {
+      # creating object to put in values of columns that need an empty cell
+      incomplete.cols <- c()
+      # finding which columns need an empty cell to be filled
+      for (i in (((length(characters)%%columns)+1):columns))
+      {
+        incomplete.cols[columns-i+1] <- (which(order(key.chars)==i))
+      }
+      # placing the empty character in the correct place of the characters object
+      for (i in (order(incomplete.cols)))
+      {
+        characters <- append(characters, "", after = (incomplete.cols[i]*ceiling(length(characters)/columns)-1))
+      }
+    }
+    # placing characters object in the encryption matrix
+    encryption.matrix <- t(matrix(characters, ncol = columns, byrow = FALSE))
+
+    # ordering the encryption matrix by the key characters
+    row.names(encryption.matrix) <- order(key.chars)
+    encryption.matrix <- encryption.matrix[order(row.names(encryption.matrix)), ]
+
+    # pasting the characters from the encryption matrix into the decrypted message
+    message <- paste(encryption.matrix, collapse = "")
   }
   return(message)
 }
-
-
-  columnar_transposition_encrypt <- function(message, key)
-  {
-    x <- strsplit(message, "")[[1]]
-    y <- nchar(key)
-    z <- strsplit(tolower(key), "")[[1]]
-    if (length(x) %% y != 0)
-    {
-      for (i in 1:(y-(length(x) %% y)))
-      {
-        x <- append(x, "")
-      }
-    }
-    x.mat <- data.frame(matrix(x, ncol = y, byrow = TRUE))
-    colnames(x.mat) <- z
-    x.mat <- as.matrix(x.mat[,order(names(x.mat))])
-    encrypted.message <- paste(x.mat, collapse = "")
-  }
-
-  columnar_transposition_decrypt <- function(message, key)
-  {
-    x <- strsplit(message, "")[[1]]
-    y <- nchar(key)
-    z <- strsplit(tolower(key), "")[[1]]
-    if (length(x) %% y != 0)
-    {
-      j <- c()
-      for (i in (((length(x)%%y)+1):y))
-      {
-        j[y-i+1] <- (which(order(z)==i))
-      }
-      for (i in (order(j)))
-      {
-        x <- append(x, "", after = (j[i]*ceiling(length(x)/y)-1))
-      }
-    }
-    x.mat <- t(matrix(x, ncol = y, byrow = FALSE))
-    row.names(x.mat) <- order(z)
-    x.mat <- x.mat[order(row.names(x.mat)), ]
-    decrypted.message <- paste(x.mat, collapse = "")
-  }
-
