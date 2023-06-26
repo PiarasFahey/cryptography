d82fbe9d5b2f332209baddfa1e96e97d09b5cb72
Piaras Fahey <faheypi@tcd.ie>
2023-06-26 18:17:19 +0100

    Initial commit

--- R/columnar_transposition.R
+++ R/columnar_transposition.R
@@ -1,19 +1,30 @@
 #' Columnar Transposition Cipher
 #'
 #' @param message A character vector
-#' @param key A character vector used as the encryption key
+#' @param key An [a-zA-Z] character vector used as the encryption key
 #' @param encrypt (Default: `TRUE`) TRUE will encrypt the message, while FALSE will decrypt the message.
 #'
-#' @return A character vector of either plaintext that has been encrypted or ciphertext that has been decrypted.
+#' @return A character vector of either plaintext that has been encrypted or ciphertext that has been decrypted
+#' using the columnar transposition cryptographic method.
 #' @export
 #'
 #' @examples columnar_transposition("Hidden message", "hack", encrypt = TRUE)
 
 columnar_transposition <- function(message, key, encrypt=TRUE) {
 
+  # stop if message is not a character vector
+  stopifnot(inherits(message,"character"))
+
+  # stop if the key is not a character vector
+  if (grepl("[a-zA-Z]", key) == FALSE) {
+    stop("key must be an [a-zA-Z] character")
+  }
+
+  # calling encryption or decryption method
   if (encrypt == TRUE) {
     message <- columnar_transposition_encrypt(message, key)
   }
+
   if (encrypt == FALSE) {
     message <- columnar_transposition_decrypt(message, key)
   }
@@ -36,7 +47,7 @@ columnar_transposition <- function(message, key, encrypt=TRUE) {
     x.mat <- data.frame(matrix(x, ncol = y, byrow = TRUE))
     colnames(x.mat) <- z
     x.mat <- as.matrix(x.mat[,order(names(x.mat))])
-    encrypted.message <- paste((x.mat), collapse = "")
+    encrypted.message <- paste(x.mat, collapse = "")
   }
 
   columnar_transposition_decrypt <- function(message, key)
@@ -51,14 +62,14 @@ columnar_transposition <- function(message, key, encrypt=TRUE) {
       {
         j[y-i+1] <- (which(order(z)==i))
       }
-      for (i in (1:length(j)))
+      for (i in (order(j)))
       {
-        x <- append(x, "", after = (floor(length(x)/y)*y+j[i]-1) )
+        x <- append(x, "", after = (j[i]*ceiling(length(x)/y)-1))
       }
     }
-    x.mat <- t(matrix(x, ncol = y, byrow = TRUE))
+    x.mat <- t(matrix(x, ncol = y, byrow = FALSE))
     row.names(x.mat) <- order(z)
     x.mat <- x.mat[order(row.names(x.mat)), ]
-    encrypted.message <- paste(t(x.mat), collapse = "")
+    decrypted.message <- paste(x.mat, collapse = "")
   }
 
--- tests/testthat/test-columnar_transposition.R
+++ tests/testthat/test-columnar_transposition.R
@@ -1,5 +1,33 @@
 
-test_that("function works when key is alphabetically ordered", {
+test_that("function works when key is alphabetically ordered and length of input is divisible by length of key", {
+  expect_equal(columnar_transposition("testing message", key  = "abc", encrypt = TRUE), "ttgeaei sgsnmse")
+  expect_equal(columnar_transposition("ttgeaei sgsnmse", key  = "abc", encrypt = FALSE), "testing message")
+  expect_equal(columnar_transposition(columnar_transposition("testing message", key  = "abc"), key = "abc", encrypt = FALSE), "testing message")
+})
+
+test_that("function works when key is not alphabetically order and length of input is divisible by length of key", {
+  expect_equal(columnar_transposition("testing message", key  = "cab", encrypt = TRUE), "ei sgsnmsettgea")
+  expect_equal(columnar_transposition("ei sgsnmsettgea", key  = "cab", encrypt = FALSE), "testing message")
+})
+
+test_that("function works when key is alphabetically ordered and length of input is not divisible by length of key", {
   expect_equal(columnar_transposition("testing basic", key = "adhkz", encrypt = TRUE), "tnsegis ctbia")
   expect_equal(columnar_transposition("tnsegis ctbia", key = "adhkz", encrypt = FALSE), "testing basic")
+})
+
+test_that("function works when key is not alphabetically ordered and length of input is not divisible by length of key", {
+  expect_equal(columnar_transposition("secretmessage", key = "sham", encrypt = TRUE), "cmaetsregsese")
+  expect_equal(columnar_transposition("cmaetsregsese", key = "sham", encrypt = FALSE), "secretmessage")
+})
+
+test_that("input text must be a character vector", {
+  expect_error(columnar_transposition(c(), "key"))
+  expect_error(columnar_transposition(matrix(c("a","b","c","d"),2,2), "key"))
+})
+
+test_that("key must be an [a-zA-Z] character vector", {
+  expect_error(columnar_transposition("very secret message!", 1.56))
+  expect_error(columnar_transposition("very secret message!", "密钥"))
+  expect_error(columnar_transposition("very secret message!", -3))
+  expect_error(columnar_transposition("very secret message!", 0))
 })
