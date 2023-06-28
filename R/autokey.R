e49f704ba87287597ee454fcdae39cb41408c08f
Piaras Fahey <faheypi@tcd.ie>
2023-06-28 20:23:16 +0100

    Added Fixed Autokey decrypt and added some tests

--- R/autokey.R
+++ R/autokey.R
@@ -11,30 +11,53 @@
 #' @export
 #'
 #' @examples autokey("VerySecretMessage", "Hack", encrypt = TRUE)
+
 autokey <- function(message, key, encrypt=TRUE)
 {
+  # calling encrypt method
   if (encrypt == TRUE)
   {
-    message <- autokey_encrypt(message, key)
+    # creating the encryption key by binding the key to the message and making it the length of the message
+    full.key <- paste(c(strsplit(key, "")[[1]], strsplit(message, "")[[1]])[1:nchar(message)], collapse = "")
+
+    # applying the Vigenere method to the message with the newly formed encryption key
+    message <- DescTools::Vigenere(message, full.key)
   }
-  if (encrypt == FALSE)
-  {
-    message <- autokey_decrypt(message, key)
+
+  # calling decrypt method
+  if (encrypt == FALSE) {
+
+    # calculating necessary number of iterations of decryption
+    iterations <- ceiling(nchar(message)/nchar(key))
+
+    # creating text blocks object to decrypt by length of key
+    text.blocks <- vector("character", iterations)
+
+    # if only 1 iteration then there will be a single text block of the message
+    if (iterations == 1) {
+      text.blocks <- message
+    }
+
+    if (iterations > 1) {
+      for (i in 1:iterations-1) {
+      text.blocks[i] <- paste(strsplit(message, "")[[1]][(1+(i-1)*nchar(key)):(i*nchar(key))], collapse = "")
+      }
+      text.blocks[iterations] <- paste(strsplit(message, "")[[1]][(1+(iterations-1)*nchar(key)):nchar(message)], collapse = "")
+    }
+
+    # creating decrypted blocks object to fill with decryption iterations
+    decrypted.blocks <- vector("character", iterations)
+
+    # decrypting first block of cipher text and putting into first block
+    decrypted.blocks[1] <- DescTools::Vigenere(text.blocks[1], key, decrypt = TRUE)
+
+    if (iterations > 1) {
+      for (i in 2:iterations) {
+      decrypted.blocks[i] <- DescTools::Vigenere(text.blocks[i], decrypted.blocks[i-1], decrypt = TRUE)
+      }
+    }
+    message <- paste(decrypted.blocks, collapse = "")
   }
   return(message)
 }
 
-
-autokey_encrypt <- function(message, key) {
-
-  # creating the encryption key by binding the key to the message and making it the length of the message
-  full.key <- paste(c(strsplit(key, "")[[1]], strsplit(message, "")[[1]])[1:nchar(message)], collapse = "")
-
-  # applying the Vigenere method to the message with the newly formed encryption key
-  encrypted.text <- DescTools::Vigenere(message, full.key)
-}
-
-
-autokey_decrypt <- function(x, key) {
-
-}
--- tests/testthat/test-autokey.R
+++ tests/testthat/test-autokey.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("encryption works properly with suitable characters", {
+  expect_equal(autokey("testing12345", key = "abc", encrypt = TRUE), DescTools::Vigenere("testing12345", key = "abctesting12"))
+  expect_equal(autokey("TeStINg12345", key = "aBC", encrypt = TRUE), DescTools::Vigenere("TeStINg12345", key = "aBCTeStINg12"))
+})
+
+test_that("decryption works properly with suitable characters", {
+  expect_equal(autokey("J5KcCVPZfZvx", key = "abc", encrypt = FALSE), "testing12345")
+
+})
+
+test_that("function deals with unsuitable characters by removing them", {
+  expect_equal(autokey(autokey("leaving 2 spaces###", key = "key123"), key = "key123", encrypt = FALSE), "leaving2spaces")
 })
