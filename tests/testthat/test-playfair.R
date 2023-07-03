52ac09e88fc9f7f1535d7ff57625a293985c334b
Piaras Fahey <faheypi@tcd.ie>
2023-07-03 17:06:16 +0100

    fixed typos in test description

--- tests/testthat/test-playfair.R
+++ tests/testthat/test-playfair.R
@@ -7,7 +7,7 @@ test_that("generating encryption matrix from encryption key works", {
                       ncol = 5, nrow = 5, byrow = TRUE))
   })
 
-test_that("encryption works for digraphs of each possible algorithm", {
+test_that("encryption works for digrams of each possible algorithm", {
   expect_equal(playfair("goaeym", "safety first", encrypt = TRUE),"HNFTDU")
   expect_equal(playfair("suyc", "safety first", encrypt = TRUE), "YSIY")
 })
@@ -17,7 +17,7 @@ test_that("encryption works for non-[a-zA-Z] characters", {
   expect_equal(playfair("s£$%^u 19y_{}@@c", "safety first", encrypt = TRUE), "YSIY")
 })
 
-test_that("decryption works for digraphs of each possible algorithm", {
+test_that("decryption works for digrams of each possible algorithm", {
   expect_equal(playfair("hnftdu", "safety first", encrypt = FALSE), "GOAEYM")
   expect_equal(playfair("YSIY", "safety first", encrypt = FALSE), "SUYC")
 })
